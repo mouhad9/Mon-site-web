1a4653f6453dbb7416c7218a3dc0918d90c3c171
mouhad9 <64971074+mouhad9@users.noreply.github.com>
2024-07-26 18:05:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function ouvrirNouvellePage(){
+    alert("Vous allez être redirigé vers le profil de Mouhad");
+    setTimeout(function() {
+        window.location.href = "Profil.html";
+    }, 1000); // Délai de 1 seconde avant la redirection
+}
